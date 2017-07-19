5152811ab2ef73baeef99ccd69e1b31611eb4b5c
Julie.Canevet <julie.canevet@hotmail.fr>
2017-07-19 11:39:27 +0200

    reglage du temps d'affichage au onclick

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-//carrousel avec les images au clique
-
+//carrousel avec les images au clic
 var mesImages = document.getElementsByTagName('IMG');
 var next = document.getElementById('apres');
 var index = 0;
 var previous = document.getElementById('avant');
 
 // Passer a l'image précédente
-    previous.onclick = function () {
+    var interval = setInterval(apres, 3000);
+
+    previous.onclick = function (){
+      clearInterval(interval);
+      avant(); 
+      interval = setInterval(apres, 3000);
+}
+
+
+    function avant() {
     index --;
    	for (var x=0; x < mesImages.length; x++){
   	mesImages[x].style.display = "none";
@@ -20,7 +28,14 @@ var previous = document.getElementById('avant');
 	}
 
 // Passer à l'image suivante
-    next.onclick = function() {
+    next.onclick = function (){
+      clearInterval(interval);
+      apres();
+      interval = setInterval(apres, 3000);
+
+    }
+
+    function apres() {
    	index++;
   	for (var x=0; x < mesImages.length; x++){
   	mesImages[x].style.display = "none";
